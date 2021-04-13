99e885b205acce72f850d2cc168dde642f0215af
bgallois <benjamin@gallois.cc>
2021-04-13 18:00:02 +0200

    [feat] add css style

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <QApplication>
+#include <QFont>
+#include <QFontDatabase>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[]) {
@@ -8,6 +10,9 @@ int main(int argc, char *argv[]) {
   w.setWindowIcon(QIcon(":/openjournal.svg"));
   w.show();
   qApp->setApplicationVersion("0.0.1");
+  QFontDatabase::addApplicationFont(":/assets/Lato.ttf");
+  QFontDatabase::addApplicationFont(":/assets/Caveat.ttf");
+  w.setStyleSheet("QWidget {font-family: 'Lato', sans-serif;}");
 
   return a.exec();
 }
