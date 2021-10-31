752b64ef48658fb0d8cb1f72f7288d8a15fa406f
bgallois <benjamin@gallois.cc>
2021-10-31 16:17:12 +0100

    [fix] fix writing permission for templates

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -446,9 +446,13 @@ void MainWindow::about() {
 }
 
 void MainWindow::insertToDoTemplate() {
-  ui->entry->insertPlainText("\n### To do list\n\n- [x] Task 1\n- [ ] Task 2\n- [ ] Task 3\n");
+  if (ui->entry->isEnabled()) {
+    ui->entry->insertPlainText("\n### To do list\n\n- [x] Task 1\n- [ ] Task 2\n- [ ] Task 3\n");
+  }
 }
 
 void MainWindow::insertTableTemplate() {
-  ui->entry->insertPlainText("\nA | B | C\n--- | --- | ---\nA1 | B1 | C1\nA2 | B2 | C2\nA3 | B3 | C3\n\n");
+  if (ui->entry->isEnabled()) {
+    ui->entry->insertPlainText("\nA | B | C\n--- | --- | ---\nA1 | B1 | C1\nA2 | B2 | C2\nA3 | B3 | C3\n\n");
+  }
 }
